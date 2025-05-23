9e4b4348afea4458054e0579170fe167b92ed71d
Diego Delgado <diegodelgado365@gmail.com>
2025-05-23 13:04:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import AgregarProducto from "./components/AgregarProducto";
